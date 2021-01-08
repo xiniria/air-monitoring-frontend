0f376694b3dca9acac4acd5f7fe5efd831d4d5fb
Ali Raiki <ali.raiki@student-cs.fr>
2021-01-08 16:33:42 +0100

    Update Material UI

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import { StylesProvider } from '@material-ui/core';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
@@ -23,46 +24,48 @@ function Navbar(): JSX.Element {
   const [value, setValue] = React.useState('recents');
 
   return (
-    <BottomNavigation
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-      className="stickToBottom"
-    >
-      <BottomNavigationAction
-        component={Link}
-        to="/location"
-        classes={classes}
-        label="Location"
-        value="location"
-        icon={<LocationOnOutlinedIcon />}
-      />
-      <BottomNavigationAction
-        component={Link}
-        to="/map"
-        classes={classes}
-        label="Carte"
-        value="map"
-        icon={<MapOutlinedIcon />}
-      />
-      <BottomNavigationAction
-        component={Link}
-        to="/predictions"
-        classes={classes}
-        label="Prédictions"
-        value="predictions"
-        icon={<BarChartOutlinedIcon />}
-      />
-      <BottomNavigationAction
-        component={Link}
-        to="/info"
-        classes={classes}
-        label="Informations"
-        value="info"
-        icon={<InfoOutlinedIcon />}
-      />
-    </BottomNavigation>
+    <StylesProvider injectFirst>
+      <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        className="stickToBottom"
+      >
+        <BottomNavigationAction
+          component={Link}
+          to="/location"
+          classes={classes}
+          label="Location"
+          value="location"
+          icon={<LocationOnOutlinedIcon />}
+        />
+        <BottomNavigationAction
+          component={Link}
+          to="/map"
+          classes={classes}
+          label="Carte"
+          value="map"
+          icon={<MapOutlinedIcon />}
+        />
+        <BottomNavigationAction
+          component={Link}
+          to="/predictions"
+          classes={classes}
+          label="Prédictions"
+          value="predictions"
+          icon={<BarChartOutlinedIcon />}
+        />
+        <BottomNavigationAction
+          component={Link}
+          to="/info"
+          classes={classes}
+          label="Informations"
+          value="info"
+          icon={<InfoOutlinedIcon />}
+        />
+      </BottomNavigation>
+    </StylesProvider>
   );
 }
 
